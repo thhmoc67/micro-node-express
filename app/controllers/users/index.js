7db1f58f6c869f3bcdfc636cce51b1cdf0962aef
Dayal <dayal.sharan@nestaway.com>
2019-03-08 16:52:22 +0530

    added dynamo

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -1,11 +1,44 @@
 var express = require("express");
 var router = express.Router();
+var AWS = require("aws-sdk");
 
+AWS.config.update({
+  region: "us-east-1",
+  endpoint: "dynamodb.us-east-1.amazonaws.com",
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
 /* GET users listing. */
-router.get("/", (req, res, next) => {
-  res.json({
-    name: 'Dayal',
-    email: 'dayal.sharan@nestassist.com'
+router.post("/new", async (req, res, next) => {
+  console.log("req body", req.body);
+  var table_name = "users";
+  var created_date = new Date().toDateString();
+
+  var params = {
+    TableName: table_name,
+    Item: {
+      id: 23,
+      created_date: created_date,
+      name: req.body.name,
+      email: req.body.email,
+    },
+  };
+
+  console.log("Adding a new user...");
+  docClient.put(params, function(err, data) {
+    if (err) {
+      console.error(
+        "Unable to add user. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+      res.json({
+        error: "something went wrong",
+        params: params,
+      });
+    } else {
+      console.log("Added user:", JSON.stringify(data, null, 2));
+      res.json(params.Item);
+    }
   });
 });
 
