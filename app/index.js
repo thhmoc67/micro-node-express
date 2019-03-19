d1f14ea6fd20f7a549fcf87a0f4dd661fd4f370e
Dayal <dayal.sharan@nestaway.com>
2019-03-19 16:35:04 +0530

    added twilio in project

--- app/index.js
+++ app/index.js
@@ -6,7 +6,6 @@ let logger = require('morgan');
 const router = require('./router');
 
 let app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
